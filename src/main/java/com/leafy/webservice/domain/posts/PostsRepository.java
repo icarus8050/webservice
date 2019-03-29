beb5bf6df00a745053c704eaf49dd9e7eb4d3c0a
leafy <icarus8050@naver.com>
2019-03-30 00:22:56 +0900

    simple jpa example
    간단한 post api 예제
    BaseTimeEntity를 활용한 등록일/수정일 자동 등록 예제

---
+++ src/main/java/com/leafy/webservice/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.leafy.webservice.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
--- src/main/java/com/leafy/webservice/web/WebRestController.java
+++ src/main/java/com/leafy/webservice/web/WebRestController.java
@@ -1,13 +1,23 @@
 package com.leafy.webservice.web;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.leafy.webservice.domain.posts.PostsRepository;
+import com.leafy.webservice.domain.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
