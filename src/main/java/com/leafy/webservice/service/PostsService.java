61c6366a01ceb03f17cfc76c38ebfa969124031f
leafy <icarus8050@naver.com>
2019-04-04 22:55:31 +0900

    simple web api

--- src/main/java/com/leafy/webservice/service/PostsService.java
+++ src/main/java/com/leafy/webservice/service/PostsService.java
@@ -1,4 +1,29 @@
 package com.leafy.webservice.service;
 
+import com.leafy.webservice.domain.posts.PostsMainResponseDto;
+import com.leafy.webservice.domain.posts.PostsRepository;
+import com.leafy.webservice.domain.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
 public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto dto) {
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/leafy/webservice/web/WebRestController.java
+++ src/main/java/com/leafy/webservice/web/WebRestController.java
@@ -2,6 +2,7 @@ package com.leafy.webservice.web;
 
 import com.leafy.webservice.domain.posts.PostsRepository;
 import com.leafy.webservice.domain.posts.PostsSaveRequestDto;
+import com.leafy.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -17,7 +18,7 @@ public class WebRestController {
     }
 
     @PostMapping("/posts")
-    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
-        postsRepository.save(dto.toEntity());
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+        return postsService.save(dto);
     }
 }
